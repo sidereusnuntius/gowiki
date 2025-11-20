f308779be2ef4fc6cf6b6bed0cebd4a876901ed6
sidereusnuntius <firewalker@posteo.net>
2025-11-20 11:47:48 -0300

    Missing interface for AP-related database methods.

---
+++ internal/db/fed.go
@@ -0,0 +1,15 @@
+package db
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/sidereusnuntius/wiki/internal/domain"
+)
+
+type Fed interface {
+	ActorIdByInbox(ctx context.Context, iri *url.URL) (*url.URL, error)
+	ActorIdByOutbox(ctx context.Context, iri *url.URL) (*url.URL, error)
+	OutboxForInbox(ctx context.Context, inboxIRI *url.URL) (*url.URL, error)
+	GetUserFed(ctx context.Context, id *url.URL) (user domain.UserFed, err error)
+}
