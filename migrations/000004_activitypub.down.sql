0f6cd1c88e128f9907db21932d6045f12a03e261
sidereusnuntius <firewalker@posteo.net>
2025-11-22 11:47:08 -0300

    feat(migrations): create tables to store AP objects

---
+++ migrations/000004_activitypub.down.sql
@@ -0,0 +1,4 @@
+DROP INDEX idx_collection_pagination;
+DROP INDEX idx_reverse_collection_lookup;
+DROP TABLE ap_collection_members;
+DROP TABLE ap_object_cache;
---
+++ migrations/000004_activitypub.up.sql
@@ -0,0 +1,33 @@
+CREATE TABLE ap_object_cache (
+    ap_id VARCHAR(255),
+
+    local_table VARCHAR(32),
+    local_id INTEGER,
+
+    type VARCHAR(32) NOT NULL,
+    raw_json TEXT,
+    inserted_at INTEGER DEFAULT (cast(strftime('%s','now') as int)) NOT NULL,
+    last_updated INTEGER,
+
+    -- Last time the object was fetched. For local objects,
+    -- last_fetched is null.
+    last_fetched INTEGER,
+
+    PRIMARY KEY (ap_id),
+    UNIQUE (local_table, local_id)
+);
+
+CREATE TABLE ap_collection_members (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    collection_ap_id VARCHAR(255) NOT NULL,
+    member_ap_id VARCHAR(255) NOT NULL,
+    inserted_at INTEGER DEFAULT (cast(strftime('%s','now') as int)) NOT NULL,
+
+    UNIQUE (collection_ap_id, member_ap_id)
+);
+
+CREATE INDEX idx_collection_pagination
+ON ap_collection_members (collection_ap_id, id DESC);
+
+CREATE INDEX idx_reverse_collection_lookup
+ON ap_collection_members (member_ap_id);
